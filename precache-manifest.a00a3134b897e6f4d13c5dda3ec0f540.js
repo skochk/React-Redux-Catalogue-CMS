fa9834d019d99d34b0697acf9a85c129c101060f
Sergey <dropmailqwade@gmail.com>
2020-08-04 09:58:37 +0300

    Updates

---
+++ precache-manifest.a00a3134b897e6f4d13c5dda3ec0f540.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315b05be22227b44eaf0ada12851a2a1",
+    "url": "/React-Redux-Catalogue-CMS/index.html"
+  },
+  {
+    "revision": "db300ba16b0a832b3279",
+    "url": "/React-Redux-Catalogue-CMS/static/css/2.f7e5fa38.chunk.css"
+  },
+  {
+    "revision": "39a076d35bbc616bdcf6",
+    "url": "/React-Redux-Catalogue-CMS/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "db300ba16b0a832b3279",
+    "url": "/React-Redux-Catalogue-CMS/static/js/2.3793f0f9.chunk.js"
+  },
+  {
+    "revision": "0281dfb583f029ca279eec67903f9f76",
+    "url": "/React-Redux-Catalogue-CMS/static/js/2.3793f0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a076d35bbc616bdcf6",
+    "url": "/React-Redux-Catalogue-CMS/static/js/main.52df60e5.chunk.js"
+  },
+  {
+    "revision": "ebd64fda7a79a39c7ba1",
+    "url": "/React-Redux-Catalogue-CMS/static/js/runtime-main.68fda5fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Redux-Catalogue-CMS/static/media/logo.5d5d9eef.svg"
+  }
+]);
